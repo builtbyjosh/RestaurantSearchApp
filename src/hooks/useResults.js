f0af3f9a93d440fb9723d4c7d1d92aa92790ffbf
builtbyjosh <pc.refuge@gmail.com>
2022-01-11 22:11:42 -0600

    code cleanup

--- src/hooks/useResults.js
+++ src/hooks/useResults.js
@@ -5,25 +5,22 @@ export default () => {
   const [results, setResults] = useState([]);
   const [errorMessage, setErrorMessage] = useState('');
 
-  const searchApi = async searchTerm => {
+  const searchApi = async (searchTerm) => {
     console.log('Hi there!');
     try {
       const response = await yelp.get('/search', {
         params: {
           limit: 50,
           term: searchTerm,
-          location: 'san jose'
-        }
+          location: 'san jose',
+        },
       });
       setResults(response.data.businesses);
     } catch (err) {
       setErrorMessage('Something went wrong');
     }
   };
-
-  // Call searchApi when component
-  // is first rendered.  BAD CODE!
-  // searchApi('pasta');
+  
   useEffect(() => {
     searchApi('pasta');
   }, []);
