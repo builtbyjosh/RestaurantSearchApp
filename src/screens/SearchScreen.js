79e5832ebcf7e6ad53e056dd8468737fd5523ace
builtbyjosh <pc.refuge@gmail.com>
2022-01-11 21:42:52 -0600

    add error handling

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,25 +1,44 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import yelp from '../api/yelp';
 import RestaurantList from '../components/RestaurantList';
 import SearchBar from '../components/SearchBar';
 
 const SearchScreen = () => {
   const [searchTerm, setSearchTerm] = useState('');
-
-
+  const [results, setResults] = useState([]);
+  const [errorMsg, setErrorMsg] = useState('')
+  
+  const fetchResults = async () => {
+    try {
+      const res = await yelp.get('/search', {
+      params: {
+        term: searchTerm,
+        limit: 50,
+        location: 'chicago'
+      }
+    })
+    setResults(await res.data.businesses)
+    setErrorMsg('')
+    } catch (err) {
+      setErrorMsg('Opps!! Something went wrong.')
+    }
+    
+  }
 
   return (
     <View>
       <SearchBar 
         searchTerm={searchTerm} 
         onTermChange={setSearchTerm} 
-        // onTermSubmit={} 
+        onTermSubmit={fetchResults} 
       />
       <RestaurantList  />
-      <Text>{searchTerm}</Text>
+      <Text>{results.length}</Text>
+      {errorMsg ? <Text>{errorMsg}</Text> : null}
     </View>
   );
-};
+}; 
 
 export default SearchScreen;
 
