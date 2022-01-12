f70266caa5acffddb1f17597c3828f18cccd2957
builtbyjosh <pc.refuge@gmail.com>
2022-01-11 21:18:59 -0600

    initial component creation

---
+++ src/components/RestaurantList.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const RestaurantList = () => {
+  return (
+    <View>
+      <Text></Text>
+    </View>
+  )
+}
+
+export default RestaurantList
+
+const styles = StyleSheet.create({})
