0a90814a67f0940caadd6494fb4c3445e1f06b2e
builtbyjosh <pc.refuge@gmail.com>
2022-01-12 21:18:00 -0600

    finish styling, add condional rendering

--- src/screens/ResultsShowScreen.js
+++ src/screens/ResultsShowScreen.js
@@ -1,13 +1,33 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React, {useState, useEffect} from 'react'
+import { StyleSheet, Text, View, FlatList, Image } from 'react-native'
 import yelp from '../api/yelp'
 
 const ResultsShowScreen = ({navigation}) => {
+  const [result, setResult] = useState(null)
   const id = navigation.getParam('id')
+
+  const fetchBusiness = async (id) => {
+    let res = await yelp.get(`/${id}`)
+    setResult(res.data)
+  }
+
+  useEffect(() => {
+    fetchBusiness(id)
+  }, [])
+  if(!result){
+    return null;
+  }
 
   return (
     <View>
-      <Text>Text</Text>
+      <Text>{result.name}</Text>
+      <FlatList
+      data={result.photos}
+      keyExtractor={(photo) => photo}
+      renderItem={({item})=>{
+        return <Image style={styles.image} source={{uri: item}} />
+      }}
+      />
     </View>
   )
 }
@@ -15,5 +35,11 @@ const ResultsShowScreen = ({navigation}) => {
 export default ResultsShowScreen
 
 const styles = StyleSheet.create({
-
+  image: {
+    height: 200,
+    width: 300,
+    marginVertical: 10,
+    borderRadius: 10,
+    alignSelf: 'center'
+  }
 })
