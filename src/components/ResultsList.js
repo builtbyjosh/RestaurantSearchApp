b13eeab74095cf2bbadeeed085f333875435ce63
builtbyjosh <pc.refuge@gmail.com>
2022-01-12 21:03:10 -0600

    create list to show items

--- src/components/ResultsList.js
+++ src/components/ResultsList.js
@@ -1,14 +1,42 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React from 'react';
+import { FlatList, StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import ResultListItem from './ResultListItem';
+import { withNavigation } from 'react-navigation';
 
-const ResultsList = () => {
+const ResultsList = ({ title, results, navigation }) => {
   return (
-    <View>
-      <Text></Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>{title}</Text>
+      
+      <FlatList 
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        data={results}
+        keyExtractor={(result)=> result.id}
+        renderItem={({item})=>{
+          return (
+            <TouchableOpacity onPress={()=>{navigation.navigate('ResultsShow',{id: item.id})}}>
+              <ResultListItem result={item} />
+            </TouchableOpacity>
+          )
+        }}
+        
+      />
     </View>
-  )
-}
+  );
+};
 
-export default ResultsList
+export default withNavigation(ResultsList);
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  title: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginLeft: 15,
+    marginBottom: 5
+  },
+  container: {
+    marginBottom: 10,
+    
+  }
+});
