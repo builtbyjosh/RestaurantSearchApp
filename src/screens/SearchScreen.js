04707958030bd148d9356f0f0715a20d00f438c1
builtbyjosh <pc.refuge@gmail.com>
2022-01-12 21:03:56 -0600

    add price filter

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,22 +1,34 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, ScrollView } from 'react-native';
+import ResultsList from '../components/ResultsList';
 import SearchBar from '../components/SearchBar';
 import useResults from '../hooks/useResults';
 
 const SearchScreen = () => {
   const [term, setTerm] = useState('');
   const [searchApi, results, errorMessage] = useResults();
+  const filterByPrice = (price) => {
+    return results.filter((result) => {
+      return result.price === price;
+    });
+  };
 
   return (
-    <View>
+    <>
       <SearchBar
         term={term}
         onTermChange={setTerm}
         onTermSubmit={() => searchApi(term)}
       />
       {errorMessage ? <Text>{errorMessage}</Text> : null}
-      <Text>We have found {results.length} results</Text>
-    </View>
+      
+      <ScrollView>
+        <ResultsList title={'Cost Effective'} results={filterByPrice('$')} />
+        <ResultsList title={'Bit Pricier'} results={filterByPrice('$$')} />
+        <ResultsList title={'Big Spender'} results={filterByPrice('$$$')} />
+        <ResultsList title={'Besoz Money'} results={filterByPrice('$$$$')} />
+      </ScrollView>
+    </>
   );
 };
 
